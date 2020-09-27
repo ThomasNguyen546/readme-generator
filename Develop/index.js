57ebebaf8dc3325efe8fe06f9c4252d8b6d82fd4
Thomas Nguyen <thomasnguyen546@gmail.com>
2020-09-27 17:29:32 -0600

    added questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,45 @@
+const inquirer = require('inquirer');
+
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please provide the name of your project.'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions for usage.'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Please provide licenses for your project.'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide any contributors of the project.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions'
+    }
 ];
 
 // function to write README file
